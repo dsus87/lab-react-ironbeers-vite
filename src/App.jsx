4d984bcfbc6375ad1fc889d68b571d416757dd0c
Daniel Susetio <dsusetio87@gmail.com>
2024-02-24 14:21:53 +0100

    iteration 1-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
 
+import React from "react";
+import { Routes, Route } from "react-router-dom"; // <== IMPORT
+
+import { BrowserRouter as Router} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AllBeersPage from "./pages/AllBeersPage";
+import RandomBeerPage from "./pages/RandomBeerPage";
+import AddBeerPage from "./pages/AddBeerPage";
+import BeerDetailsPage from "./pages/BeerDetailsPage";
+
 function App() {
   return (
     <div className="App">
       <h1>LAB | React IronBeers</h1>
+
+      <Routes> 
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/" element={ <AllBeersPage /> } />
+        <Route path="/" element={ <RandomBeerPage /> } />
+        <Route path="/" element={ <AddBeerPage/> } />
+        <Route path="/" element={ <BeerDetailsPage/> } />
+      </Routes>
+
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,36 @@
-function HomePage() {}
+import React from 'react';
 
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h2>Welcome to IronBeers!</h2>
+      <p>Discover the world of beers with us.</p>
+      <div className="links">
+        <a href="/beers">
+          <div className="link-card">
+            <img src="src/assets/beers.png" alt="All Beers" />
+            <h3>All Beers</h3>
+            <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p1>
+          </div>
+        </a>
+        <a href="/random-beer">
+          <div className="link-card">
+            <img src="src/assets/random-beer.png" alt="Random Beer" />
+            <h3>Random Beer</h3>
+            <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p1>
+
+          </div>
+        </a>
+        <a href="/new-beer">
+          <div className="link-card">
+            <img src="src/assets/new-beer.png" alt="New Beer" />
+            <h3>New Beer</h3>
+            <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p1>
+
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
 export default HomePage;
